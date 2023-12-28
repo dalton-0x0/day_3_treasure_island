ef096dfc895c97453ca2cd979c289889740cfe7b
Dalton Okechukwu <dalton.0x0@gmail.com>
2023-12-28 00:28:14 -0700

    adds readme and flowchart. updates main.py code

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+print(r'''
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                  _.--.
+              _.-'_:-'||
+          _.-'_.-::::'||
+     _.-:'_.-::::::'  ||
+   .'`-.-:::::::'     ||
+  /.'`;|:::::::'      ||_
+ ||   ||::::::'     _.;._'-._
+ ||   ||:::::'  _.-!oo @.!-._'-.
+ \'.  ||:::::.-!()oo @!()@.-'_.|
+  '.'-;|:.-'.&$@.& ()$%-'o.'\U||
+    `>'-.!@%()@'@_%-'_.-o _.|'||
+     ||-._'-.@.-'_.-' _.-o  |'||
+     ||=[ '-._.-\U/.-'    o |'||
+     || '-.]=|| |'|      o  |'||
+     ||      || |'|        _| ';
+     ||      || |'|    _.-'_.-'
+     |'-._   || |'|_.-'_.-'
+     '-._'-.|| |' `_.-'
+          '-.||_/.-'
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-----Welcome to Treasure Island-----
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+''')
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+print("Your mission is to find the treasure and have fun!")
 
+choice1 = input('You\'re at a crossroad, where do you want to go? '
+                'Type "l" for left or "r" for right: \n').lower()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if choice1 == "l":
+    choice2 = input(
+        'You\'re at a lake with an island in the middle. '
+        'Type "w" to wait for a boat or "s" to swim across: \n').lower()
+    if choice2 == "w":
+        choice3 = input(
+            "You arrive at the island unharmed. "
+            "There is a house with 3 doors. One red, one yellow and one blue. "
+            "Type a door color to enter: \n").lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over!")
+        elif choice3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over!")
+        else:
+            print("You chose a door that doesn't exist. Game Over!")
+    else:
+        print("You get attacked by an angry trout. Game Over!")
+else:
+    print("You fell into a hole. Game Over!")
